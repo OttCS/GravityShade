cd76880056ad610b749d6ad141c5076214dae5b8
OttCS <gravity10.shaders@gmail.com>
2022-02-03 20:13:25 -0500

    Waving fix

--- shaders/program/gbuffers_terrain.glsl
+++ shaders/program/gbuffers_terrain.glsl
@@ -68,24 +68,24 @@ uniform mat4 gbufferModelViewInverse;
 const float PI = 3.14;
 float pi2wt = (150.79*frameTimeCounter);
 
-vec3 calcWave(in vec3 pos, in float fm, in float mm, in float ma, in float f0, in float f1, in float f2, in float f3, in float f4, in float f5) {
-	float magnitude = sin(pi2wt*fm + dot(pos, vec3(0.5))) * mm + ma;
-	vec3 d012 = sin(vec3(f0, f1, f2)*pi2wt);
+// vec3 calcWave(in vec3 pos, in float fm, in float mm, in float ma, in float f0, in float f1, in float f2, in float f3, in float f4, in float f5) {
+// 	float magnitude = sin(pi2wt*fm + dot(pos, vec3(0.5))) * mm + ma;
+// 	vec3 d012 = sin(vec3(f0, f1, f2)*pi2wt);
 	
-    vec3 ret;
-		 ret.x = pi2wt*f3 + d012.x + d012.y - pos.x + pos.z + pos.y;
-		 ret.z = pi2wt*f4 + d012.y + d012.z + pos.x - pos.z + pos.y;
-		 ret.y = pi2wt*f5 + d012.z + d012.x + pos.z + pos.y - pos.y;
-		 ret = sin(ret)*magnitude;
+//     vec3 ret;
+// 		 ret.x = pi2wt*f3 + d012.x + d012.y - pos.x + pos.z + pos.y;
+// 		 ret.z = pi2wt*f4 + d012.y + d012.z + pos.x - pos.z + pos.y;
+// 		 ret.y = pi2wt*f5 + d012.z + d012.x + pos.z + pos.y - pos.y;
+// 		 ret = sin(ret)*magnitude;
 	
-    return ret;
-}
+//     return ret;
+// }
 
-vec3 calcMove(in vec3 pos, in float f0, in float f1, in float f2, in float f3, in float f4, in float f5, in vec3 amp1, in vec3 amp2) {
-    vec3 move1 = calcWave(pos      , 0.0027, 0.0400, 0.0400, 0.0127, 0.0089, 0.0114, 0.0063, 0.0224, 0.0015) * amp1;
-	vec3 move2 = calcWave(pos+move1, 0.0348, 0.0400, 0.0400, f0, f1, f2, f3, f4, f5) * amp2;
-    return move1+move2;
-}
+// vec3 calcMove(in vec3 pos, in float f0, in float f1, in float f2, in float f3, in float f4, in float f5, in vec3 amp1, in vec3 amp2) {
+//     vec3 move1 = calcWave(pos      , 0.0027, 0.0400, 0.0400, 0.0127, 0.0089, 0.0114, 0.0063, 0.0224, 0.0015) * amp1;
+// 	vec3 move2 = calcWave(pos+move1, 0.0348, 0.0400, 0.0400, f0, f1, f2, f3, f4, f5) * amp2;
+//     return move1+move2;
+// }
 
 // VOID MAIN //
 
@@ -95,12 +95,11 @@ void main() {
     texcoord = (gl_TextureMatrix[0] * gl_MultiTexCoord0).xy;
 	lmcoord = (gl_TextureMatrix[1] * gl_MultiTexCoord1).xy;
 	vec3 position = mat3(gbufferModelViewInverse) * (gl_ModelViewMatrix * gl_Vertex).xyz + gbufferModelViewInverse[3].xyz;
-    vec3 vworldpos = position.xyz + cameraPosition.xyz;
+    // vec3 vworldpos = position.xyz + cameraPosition.xyz;
 
     mat = 0.0;
-    if (mcID == 10031.0) {
-        position.xz += calcMove(vworldpos.xyz, 0.0041, 0.0070, 0.0044, 0.0038, 0.0063, 0.0000, vec3(3.0,1.6,3.0), vec3(0.0));
-    }
+    // if (mcID == 10031.0) { // Waving in progress
+    // }
     if(mcID == 10079.0) {
 		mat = 2.0;
 	} else if(mcID == 10042.0 || mcID == 10169.0) { // POLISHED BLOCKS
